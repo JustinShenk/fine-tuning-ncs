8cca2a13be6b02bea1b4bbeb52cff1e2e9991a41
Justin Shenk <shenk.justin@gmail.com>
2017-12-01 04:50:31 +0100

    Fix output

--- get_slim_repo.sh
+++ get_slim_repo.sh
@@ -2,7 +2,4 @@
 
 # git clone or wget
 wget https://github.com/tensorflow/models/archive/master.zip -O models.zip 
-unzip models.zip
-# add it to Python PATH
-export PYTHONPATH=$PYTHONPATH:$PWD/models-master/slim
-# now we are good to call `python mytensorflow.py`
+unzip models.zip -d models
