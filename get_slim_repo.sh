561f0e98c002949d977a088d85169719336c453e
Justin Shenk <shenk.justin@gmail.com>
2017-12-12 16:17:32 +0100

    Fix unzip output file name

--- get_slim_repo.sh
+++ get_slim_repo.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 # git clone or wget
-wget https://github.com/tensorflow/models/archive/master.zip -O models.zip 
-unzip models.zip -d models
+wget https://github.com/tensorflow/models/archive/master.zip -O models.zip &&
+unzip models.zip &&
+mv models-master models && 
+rm models.zip
